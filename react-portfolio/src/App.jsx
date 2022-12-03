0a63d8edcd3530f01ed82e5eda10df3bfda5cb61
Lukmon492 <lukmon492@gmail.com>
2022-12-03 23:53:06 +0100

    Components updated

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,7 +1,15 @@
-import React from "react";
-
+import Navbar from "./scenes/Navbar";
+import { useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
 const App = () => {
-  return <div>Hello Lukman</div>;
+  const [selectedPage, setSelectedPage] = useState("home");
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+  return (
+    <div className="app bg-deep-blue">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
 };
 
 export default App;
