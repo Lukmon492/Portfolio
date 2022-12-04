dd8b7b567166e227ab8bbe5dbbbbcb6d383bade9
Lukmon492 <lukmon492@gmail.com>
2022-12-04 12:47:16 +0100

    progress

--- react-portfolio/src/scenes/Landing.jsx
+++ react-portfolio/src/scenes/Landing.jsx
@@ -1,5 +1,26 @@
-const Landing = () => {
-  return <div></div>;
+import useMediaQuery from "../hooks/useMediaQuery";
+import { motion } from "framer-motion";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+const Landing = ({ selectedPage }) => {
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px");
+  return (
+    <section
+      id="home"
+      className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10 "
+    >
+      {/* IMAGE SECTION */}
+      <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-15 md:mt-32">
+        {isAboveMediumScreens ? (
+          <div className="relative">
+            <img src="assets/rounded_corners.png" alt="profile" />
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default Landing;
