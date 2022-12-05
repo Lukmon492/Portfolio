336ca4049a9f79e18765aee7d30d3041e68bbe26
Lukmon492 <lukmon492@gmail.com>
2022-12-06 00:17:53 +0100

    component modified

--- react-portfolio/src/scenes/MySkills.jsx
+++ react-portfolio/src/scenes/MySkills.jsx
@@ -3,7 +3,34 @@ import useMediaQuery from "../hooks/useMediaQuery";
 import { motion } from "framer-motion";
 
 const MySkills = () => {
-  return <div></div>;
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  return (
+    <section id="skills" className="pt-10 pb-24">
+      {/* HEADER AND IMAGE SECTION */}
+      <div className="md:flex md:justify-between md:gap-16 mt:32">
+        <motion.div
+          className="md:w-1/3"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <p className="font-playfair font-semibold text-4xl mb-5">
+            My <span className="text-red">Skills</span>
+          </p>
+          <LineGradient className="w-1/3" />
+          <p className="mt-10 mb-7">
+            Aliquam, amet dui feugiat facilisi dui. Aliquam aliquet integer ut
+            fames odio in at.
+          </p>
+        </motion.div>
+      </div>
+    </section>
+  );
 };
 
 export default MySkills;
